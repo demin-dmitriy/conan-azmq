83defccbad3c2d678fd519bcd53214c7b5519d83
SSE4 <tomskside@gmail.com>
2018-02-21 21:20:47 +0700

    - force libstdc++11

--- build.py
+++ build.py
@@ -7,4 +7,13 @@ if __name__ == "__main__":
 
     builder = build_template_header_only.get_builder()
 
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if settings['compiler'] == 'gcc':
+            if settings['compiler.libcxx'] == 'libstdc++11':
+                filtered_builds.append([settings, options, env_vars, build_requires])
+        else:
+            filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
+
     builder.run()
