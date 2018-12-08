181fa19d196878c8c36d40c1af4ef72391684136
Demin Dmitriy <demindf@gmail.com>
2018-12-08 19:21:35 +0300

    update to use boost 1.67 and azmq from master

--- conanfile.py
+++ conanfile.py
@@ -7,53 +7,43 @@ import os
 
 
 class AZMQConan(ConanFile):
-    name = "azmq"
-    version = "1.0.2"
-    url = "https://github.com/bincrafters/conan-azmq"
-    homepage = "https://github.com/zeromq/azmq"
-    description = "C++ language binding library integrating ZeroMQ with Boost Asio"
-    license = "BSL-1.0"
-    exports = ["LICENSE.md"]
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    name = 'azmq'
+    version = 'a8f54cc8c9672da7d6301fe1d2719b1436ab4816'
+    url = 'https://github.com/bincrafters/conan-azmq'
+    homepage = 'https://github.com/zeromq/azmq'
+    description = 'C++ language binding library integrating ZeroMQ with Boost Asio'
+    license = 'BSL-1.0'
+    exports = ['LICENSE.md']
+    exports_sources = ['CMakeLists.txt']
+    generators = 'cmake'
+    settings = 'os', 'arch', 'compiler', 'build_type'
+    source_subfolder = 'source_subfolder'
+    build_subfolder = 'build_subfolder'
 
     def requirements(self):
-        self.requires.add('zmq/4.2.2@bincrafters/stable')
-        self.requires.add('boost_system/1.66.0@bincrafters/stable')
-        self.requires.add('boost_log/1.66.0@bincrafters/stable')
-        self.requires.add('boost_date_time/1.66.0@bincrafters/stable')
-        self.requires.add('boost_thread/1.66.0@bincrafters/stable')
-        self.requires.add('boost_chrono/1.66.0@bincrafters/stable')
-        self.requires.add('boost_regex/1.66.0@bincrafters/stable')
-        self.requires.add('boost_random/1.66.0@bincrafters/stable')
-        self.requires.add('boost_asio/1.66.0@bincrafters/stable')
-        self.requires.add('boost_logic/1.66.0@bincrafters/stable')
-        self.requires.add("cmake_findboost_modular/1.66.0@bincrafters/stable")
+        self.requires.add('zmq/4.2.5@bincrafters/stable')
+        self.requires.add('boost/1.67.0@conan/stable')
+
+        # It seems that package cmake_findboost_modular doesn't really depend on
+        # exact boost version, so it should be okay to use the older one.
+        self.requires.add('cmake_findboost_modular/1.66.0@bincrafters/stable')
 
     def source(self):
-        source_url = "https://github.com/zeromq/azmq"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+        source_url = 'https://codeload.github.com/zeromq/azmq'
+        tools.get('{0}/zip/{1}'.format(source_url, self.version))
+        extracted_dir = self.name + '-' + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
         # ensure our FindBoost.cmake is being used
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
-                              'set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/config")',
-                              '#set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/config")')
+        tools.replace_in_file(
+            os.path.join(self.source_subfolder, 'CMakeLists.txt'),
+            'set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/config")',
+            '#set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/config")'
+        )
 
-        # disable tests, 1.0.2 doesn't have AZMQ_NO_TESTS yet...
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
-                              'add_subdirectory(test)',
-                              '#add_subdirectory(test)')
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
-                              'add_subdirectory(doc)',
-                              '#add_subdirectory(doc)')
-    
     def _configure_cmake(self):
         cmake = CMake(self)
+        cmake.definitions['AZMQ_NO_TESTS'] = True
         cmake.configure(build_folder=self.build_subfolder)
         return cmake
                               
@@ -64,7 +54,7 @@ class AZMQConan(ConanFile):
     def package(self):
         cmake = self._configure_cmake()
         cmake.install()
-        self.copy(pattern="LICENSE-BOOST_1_0", dst='licenses', src=self.source_subfolder)
+        self.copy(pattern='LICENSE-BOOST_1_0', dst='licenses', src=self.source_subfolder)
 
     def package_info(self):
         self.info.header_only()
